70134813be303951714b4eb956c773f133803c61
Pkrishon <Kpinkins@loyola.edu>
2024-11-10 14:58:32 -0500

    Updated Code

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 
+
+
+
+
 import os
 
 def read_file_name():
@@ -9,12 +13,54 @@ def read_file_name():
 
 
 
-def read_file():
-    f_name = read_file_name()
-    fd = open(f_name, 'r')
-    values = fd.readlines()
-    fd.close()
-    return values
+def read_file(file_name):
+    input_file = open(file_name, "r")
+    try:
+        data = input_file.readlines()
+        input_file.close()
+        return data
+    except:
+        input_file.close()
+        print("File not found.")
+        return[]
+
+def name_sign(file_name):
+    name_list = []
+    open_file = open(file_name, "r")
+    try:
+        name_list = open_file.readlines()
+        open_file.close()
+        return name_list
+    except:
+        open_file.close()
+        print("File not found.")
+        return[]
+
+
+def table_seating(filename):
+    tables = 1
+    seat = 1
+    for names in name_sign(filename):
+            seat_chart = print(f'{"~" * 20}\nTable{tables:<1}, Seat{seat:<1}, {names:<1}\n{"~" * 20}')
+            seat += 1
+            if seat == 6:
+                tables += 1
+                seat = 1
+    print(seat_chart)
+    return
+
+
+
+
+def main():
+    filename = read_file_name()
+    #names = read_file(filename)
+    name_sign(filename)
+    table_seating(filename)
+
+    return
+
+main()
 
 
 
